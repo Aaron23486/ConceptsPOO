f5a336f85e656841674802953ae3f0a03208b886
Aaron23486 <aaron20770@gmail.com>
2024-03-14 21:27:10 -0600

    Finish Date Class

---
+++ ConceptsPOO/ConceptsPOO/DayException.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptsPOO
+{
+    [Serializable]
+    internal class DayException : Exception
+    {
+        public DayException()
+        {
+        }
+
+        public DayException(string? message) : base(message)
+        {
+        }
+
+        public DayException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+#pragma warning disable SYSLIB0051 // Type or member is obsolete
+        public DayException(SerializationInfo info, StreamingContext context) : base(info, context)
+#pragma warning restore SYSLIB0051 // Type or member is obsolete
+        {
+        }
+    }
+}
