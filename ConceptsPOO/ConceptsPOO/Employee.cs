0dc6be3cfa32bff4525dd45a15854e20a15135be
Aaron23486 <aaron20770@gmail.com>
2024-03-15 11:40:02 -0600

    Employee & SalaryEmployee & ComissionEmployee finish

---
+++ ConceptsPOO/ConceptsPOO/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }  // Cambiado de Date a DateTime
+
+        public DateTime HiringDate { get; set; }  // Cambiado de Date a DateTime
+
+        public bool IsActive { get; set; }
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName}, " +
+                $"Birth: {BirthDate.ToShortDateString()}, " +
+                $"Hiring: {HiringDate.ToShortDateString()}, " +
+                $"Is Active: {IsActive}";
+        }
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -4,33 +4,37 @@ using ConceptsPOO;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace ConceptsPOO
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-Console.WriteLine("POO Concepts");
-Console.WriteLine("===============");
-            SalaryEmployee employee1 = new SalaryEmployee()
-{
-                Id = 1010,
-                FirstName = "Sandra",
-                LastName = "Morales",
-                BirthDate = new DateTime(1990, 5, 23),
-                HiringDate = new DateTime(2022, 1, 15),
-                IsActive = true,
-        Salary = 1815453.45M,
-            };
-            Console.WriteLine(employee1);
+ class Program
+ {
+  static void Main(string[] args)
+   {
+     Console.WriteLine("POO Concepts");
+     Console.WriteLine("===============");
 
-       Employee employee2 = new CommissionEmploye()
+       SalaryEmployee employee1 = new SalaryEmployee()
+      {
+        Id = 1010,
+        FirstName = "Sandra",
+        LastName = "Morales",
+        BirthDate = new DateTime(1990, 5, 23),
+        HiringDate = new DateTime(2022, 1, 15),
+        IsActive = true,
+        Salary = 1815453.45M,
+      };
+         Console.WriteLine(employee1);
+
+       Employee employee2 = new CommissionEmployee()
        {
-           Id = 1010,
-           FirstName = "Sandra",
-           LastName = "Morales",
+         Id = 1010,
+           FirstName = "Patricia",
+           LastName = "Gutierrez",
            BirthDate = new DateTime(1990, 5, 23),
-           HiringDate = new DateTime(2022, 1, 15),
+           HiringDate = new DateTime(2022, 2, 5),
            IsActive = true,
+           Sales = 320000000M,
+           CommissionPercentaje = 0.03F
        };
+            Console.WriteLine(employee2);
         }
-}
+  } 
 }
